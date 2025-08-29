1f9e95597a1c51b6cc8edf533ae7070f3fbaae6b
ritesh <ritesh@gmail.com>
2025-08-29 22:56:32 +0530

    keycloack to eureka

--- discoveryserver/src/main/java/com/DiscoveryServerApplication.java
+++ discoveryserver/src/main/java/com/DiscoveryServerApplication.java
@@ -4,11 +4,13 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaServer   // creates a discovery server
+@ComponentScan(basePackages = {"com", "config"})
 public class DiscoveryServerApplication {
-    public  static void main(String [] args){
+    public static void main(String[] args){
         SpringApplication.run(DiscoveryServerApplication.class, args);
     }
 }
